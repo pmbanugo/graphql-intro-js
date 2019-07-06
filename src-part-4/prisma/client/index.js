5746c5e9aac09955172f5580da0d078b9c51e50e
Peter Mbanugo <chuksgerrard@yahoo.com>
2019-07-06 17:41:35 +0100

    completed part-4

---
+++ src-part-4/prisma/client/index.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "Book",
+    embedded: false
+  },
+  {
+    name: "Author",
+    embedded: false
+  },
+  {
+    name: "User",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `https://us1.prisma.sh/peter-mbanugo-715cd8/graphql-intro/dev`
+});
+exports.prisma = new exports.Prisma();
